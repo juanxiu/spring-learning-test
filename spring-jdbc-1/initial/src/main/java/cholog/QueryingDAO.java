7eea07fc095d3c2515187f55f13987e05e913f82
juanxiu <2316007@ewhain.net>
2024-05-11 15:45:56 +0900

    학습테스트 jdbc Templates

--- spring-jdbc-1/initial/src/main/java/cholog/QueryingDAO.java
+++ spring-jdbc-1/initial/src/main/java/cholog/QueryingDAO.java
@@ -1,5 +1,6 @@
 package cholog;
 
+import jakarta.annotation.Nullable;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
@@ -14,7 +15,7 @@ public class QueryingDAO {
         this.jdbcTemplate = jdbcTemplate;
     }
 
-    /*
+    //RowMapper 인터페이스. ResultSet의 각 행을 Customer 객체로 매핑하는 데 사용되며, 'resultSet' 에서 각 열의 값을 가져와 'Customer' 객체를 생성
     private final RowMapper<Customer> actorRowMapper = (resultSet, rowNum) -> {
         Customer customer = new Customer(
                 resultSet.getLong("id"),
@@ -23,49 +24,75 @@ public class QueryingDAO {
         );
         return customer;
     };
-    추후 rowMapper에 대해 학습해보고 이용해보기
-    */
 
-    /**
-     * public <T> T queryForObject(String sql, Class<T> requiredType)
-     */
-    public int count() {
-        //TODO : customers 디비에 포함되어있는 row가 몇개인지 확인하는 기능 구현
-        return 0;
+
+    //메서드 queryForObject는 SQL 쿼리를 실행하여 결과를 반환. 두 번째 매개변수로 필요한 반환 형식을 전달
+    public <T> T queryForObject(String sql, Class<T> requiredType) {
+        return jdbcTemplate.queryForObject(sql, requiredType);
     }
 
-    /**
-     * public <T> T queryForObject(String sql, Class<T> requiredType, @Nullable Object... args)
-     */
+        //메서드 count는 'customers' 테이블에서 행의 수를 셈. 이를 위해 queryForObject 메서드를 사용하여 "select count(*) from customers" 쿼리를 실행하고 결과를 정수로 반환함.
+    public int count() {
+        //TODO : customers 디비에 포함되어있는 row가 몇개인지 확인하는 기능 구현
+        int rowCount= jdbcTemplate.queryForObject("select count(*) from customers", Integer.class);
+        return rowCount;
+    }
+
+    // 메서드 queryForObject는 SQL 쿼리를 실행하여 결과를 반환. SQL 쿼리(sql), 반환되는 값의 타입(requiredType), 필요한 경우 쿼리에 전달되는 인자(args)들을 인수로 받음.
+    public <T> T queryForObject(String sql, Class<T> requiredType, @Nullable Object... args){
+        return jdbcTemplate.queryForObject(sql, requiredType, args);
+    }
+
+    /*메서드 getLastName은 주어진 ID에 해당하는 고객의 성(lastName)을 검색. queryForObject 메서드를 호출하여 ID를 기반으로 한 SQL 쿼리를 실행하고 결과로부터 성(lastName)을 반환
+    ?는 placeholder로, 나중에 ID 값으로 대체됨. String.class는 쿼리 결과가 문자열로 반환될 것. 메서드에 전달된 id 값이 해당 위치에 대체됨.*/
     public String getLastName(Long id) {
         //TODO : 주어진 Id에 해당하는 customers의 lastName을 반환
-        return null;
+        String lastName = jdbcTemplate.queryForObject("select last_name from customers where id = ?", String.class, id);
+
+        return lastName;
     }
 
     /**
      * public <T> T queryForObject(String sql, RowMapper<T> rowMapper, @Nullable Object... args)
      */
+
+    //RowMapper 이용.
     public Customer findCustomerById(Long id) {
-        String sql = "select id, first_name, last_name from customers where id = ?";
         //TODO : 주어진 Id에 해당하는 customer를 객체로 반환
-        return null;
+        Customer customer = jdbcTemplate.queryForObject(
+                "select id, first_name, last_name from customers where id = ?",
+                actorRowMapper,
+                id
+        );
+        return customer;
+
     }
 
     /**
      * public <T> List<T> query(String sql, RowMapper<T> rowMapper)
      */
+
+    //RowMapper 이용.
     public List<Customer> findAllCustomers() {
-        String sql = "select id, first_name, last_name from customers";
         //TODO : 저장된 모든 Customers를 list형태로 반환
-        return null;
+        List<Customer> customers = jdbcTemplate.query("select id, first_name, last_name from customers where first_name = ?",
+                actorRowMapper);
+
+        return customers;
     }
 
     /**
      * public <T> List<T> query(String sql, RowMapper<T> rowMapper, @Nullable Object... args)
      */
+
+    //RowMapper 이용.
     public List<Customer> findCustomerByFirstName(String firstName) {
-        String sql = "select id, first_name, last_name from customers where first_name = ?";
         //TODO : firstName을 기준으로 customer를 list형태로 반환
-        return null;
+        List<Customer> customers = jdbcTemplate.query("select id, first_name, last_name from customers where first_name = ?",
+                actorRowMapper,
+                firstName);
+
+        return customers;
     }
+
 }
--- spring-jdbc-1/initial/src/main/java/cholog/UpdatingDAO.java
+++ spring-jdbc-1/initial/src/main/java/cholog/UpdatingDAO.java
@@ -33,26 +33,44 @@ public class UpdatingDAO {
      */
     public void insert(Customer customer) {
         //todo: customer를 디비에 저장하기
+        String sql = "insert into customers (first_name, last_name) values (?, ?)";
+        jdbcTemplate.update(sql, customer.getFirstName(), customer.getLastName());
+
     }
     /**
      * public int update(String sql, @Nullable Object... args)
      */
     public int delete(Long id) {
         //todo: id에 해당하는 customer를 지우고, 해당 쿼리에 영향받는 row 수반환하기
-        return 0;
+        String sql = "delete from customers where id = ?";
+        return jdbcTemplate.update(sql, Long.valueOf(id));
+
     }
 
     /**
      * public int update(final PreparedStatementCreator psc, final KeyHolder generatedKeyHolder)
      */
+
+    // insertWithKeyHolder 메소드: 데이터베이스에 새로운 레코드를 삽입하고, 삽입된 레코드의 primary key 값을 반환하기 위해 사용
     public Long insertWithKeyHolder(Customer customer) {
         String sql = "insert into customers (first_name, last_name) values (?, ?)";
         KeyHolder keyHolder = new GeneratedKeyHolder();
 
         //todo : keyHolder에 대해 학습하고, Customer를 저장후 저장된 Customer의 id를 반환하기
 
-        Long id = keyHolder.getKey().longValue();
 
-        return keyHolder.getKey().longValue();
+        jdbcTemplate.update(connection -> { // 'jdbcTemplate.update 메서드를 호출. 람다식을 통해 'Connecton' 객체를 얻고 쿼리 실행.
+            PreparedStatement ps = connection.prepareStatement(  //Connection 객체를 통해 PreparedStatement 객체를 생성
+                    "insert into customers (first_name, last_name) values (?, ?)",
+                    new String[]{"id"}); //쿼리 실행 후 생성된 키를 얻기 위해 사용할 열. id열 지정.
+            ps.setString(1, customer.getFirstName()); //첫 번째 ?에 customer 객체의 firstName 값을 바인딩
+            ps.setString(2, customer.getLastName()); //두 번째 ?에 customer 객체의 lastName 값을 바인딩
+            return ps;
+        }, keyHolder); // 생성된 키를 keyHolder 에 저장.
+
+        Long id = keyHolder.getKey().longValue(); //저장된 키를 얻기 위해 keyHolder에서 Key를 가져온 다음, 그 값을 Long 형태로 변환하여 id 변수에 저장
+
+
+        return keyHolder.getKey().longValue(); //저장된 키를 반환. 새로 삽입된 고객의 id.
     }
 }
